f90e512fe9e5fc3d117bbe40bf48d1cb73ab2c1d
Laurens-Wuyts <laurens.wuyts@gmail.com>
2014-10-19 15:51:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>Control my PC</title>
+</head>
+
+<body>
+	<form name="form" action="client.py" method="POST" autocomplete="off">
+		<h1>Text to send:</h1><br>
+		<input type="text" name="post" >
+		<input type="submit" value="Send" name="submit1">
+		<input type="submit" value="YouTube" name="youtube"> <br><br>
+		<input type="submit" value="   MNM   " name="MNM"> 
+		<input type="submit" value="MNM-hits" name="MNM-hits"> 
+	</form>
+</body>
+
+</html>
